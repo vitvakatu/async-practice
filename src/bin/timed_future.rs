f5437d8da2b5538993098639d68f6db33fd1fcc7
Ilya Bogdanov <fumlead@gmail.com>
2022-11-23 19:15:50 +0400

    Solve timed_future

--- src/bin/timed_future.rs
+++ src/bin/timed_future.rs
@@ -7,14 +7,45 @@ use std::time::Duration;
 
 use std::time::Instant;
 
+struct TimedFuture<T> {
+    inner: BoxFuture<'static, T>,
+    start: Option<Instant>,
+}
+
+impl<T> TimedFuture<T> {
+    fn new(future: impl Future<Output = T> + 'static + Send) -> Self {
+        Self {
+            inner: future.boxed(),
+            start: None,
+        }
+    }
+}
+
+impl<T> Future for TimedFuture<T> {
+    type Output = (T, Duration);
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        if self.start.is_none() {
+            self.start = Some(Instant::now());
+        }
+        match self.inner.as_mut().poll(cx) {
+            Poll::Ready(value) => {
+                let elapsed = self.start.unwrap().elapsed();
+                Poll::Ready((value, elapsed))
+            }
+            Poll::Pending => Poll::Pending,
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     // Хранить футуру лучше в виде трейт-объекта BoxFuture
     // Сконструировать можно через future.boxed() для любого future: Future
 
     // Вот так измерять время:
-    let now = Instant::now();
-    let elapsed = now.elapsed();
+    // let now = Instant::now();
+    // let elapsed = now.elapsed();
 
     // let mut self = self;
     // let this = &mut *self;
