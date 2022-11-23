af9825b3568242eb4faab85af2b4f0b6c09c0c4a
Ilya Bogdanov <fumlead@gmail.com>
2022-11-23 21:15:51 +0400

    Second lesson

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
 mod executor;
 mod timer_future;
+use futures::join;
 
+use executor::*;
 use std::time::Duration;
 use timer_future::TimerFuture;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let future = TimerFuture::new(Duration::from_millis(1500));
     println!("Executing...");
-    future.await;
+    let (executor, spawner) = create_executor();
+    spawner.spawn(future);
+
+    spawner.spawn(async move {
+        let y = async {
+            println!("10");
+        };
+        let x = async {
+            println!("20");
+        };
+        (x.await, y.await);
+    });
+
+    drop(spawner);
+
+    executor.run();
+
     println!("Done");
 }
